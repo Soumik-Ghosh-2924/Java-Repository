e828cd951510a29068dc868f71c3dacd74d32381
Soumik Ghosh <115470141+Soumik-Ghosh-2924@users.noreply.github.com>
2024-05-22 19:54:08 +0530

    Add files via upload

---
+++ Fibonacci/fibonacci_dp.java
@@ -0,0 +1,36 @@
+//This is the optimized code foe the fibonacci series 
+// here the auxillary space complexity is O(n)
+//& the time complexity is O(n);
+
+
+import java.util.*;
+public class fibonacci_dp 
+{
+	static int fib(int n)
+	{
+		if(n==0 || n==1)
+			return n;
+		
+		int []a= new int[n+2];
+		a[0]=0;
+		a[1]=1;
+		int i;
+		for(i=2;i<=n;i++)
+		{
+			
+			a[i]=a[i-1]+a[i-2];
+		}
+		return a[n];
+	}
+	
+	public static void main(String args[])
+	{
+		int n=9;
+		int res=fib(n);
+		
+		System.out.println(res);
+		
+	}
+	
+
+}
